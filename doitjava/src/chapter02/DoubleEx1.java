f196f1479c22933d6d51666140c3383fce9e83f3
namwookyung <namwookyung.dev@gmail.com>
2020-08-14 13:01:28 +0900

    chapter02 - 실수 자료형 예제

---
+++ doitjava/src/chapter02/DoubleEx1.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class DoubleEx1 {
+
+	public static void main(String[] args) {
+		double dNum = 3.14123456789012345;
+		float fNum = 3.14123456789012345f;
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
+	}
+
+}
