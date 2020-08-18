2598fc93e9ee90bfa4cf66a0fd857d90ec27d504
namwookyung <namwookyung.dev@gmail.com>
2020-08-18 16:22:32 +0900

    20-08-18

--- doitjava/src/chapter03/OperationEx4.java
+++ doitjava/src/chapter03/OperationEx4.java
@@ -9,6 +9,11 @@ public class OperationEx4 {
 		ch = (fatherAge > motherAge) ? 'T' : 'F';
 		
 		System.out.println(ch);
+		
+		int num = 15;
+		boolean isEven;
+		isEven = (num %2) == 0 ? true : false;
+		System.out.println("Â¦¼öÀÎ°¡¿ä?" + (isEven?"Â¦¼ö" : "È¦¼ö"));
 	}
 
 }
