0e861b615ffd24775497744eed940c56585c61be
namwookyung <namwookyung.dev@gmail.com>
2020-08-19 15:57:38 +0900

    if-else if-else문의 예제

--- doitjava/src/chapter04/IfEx2.java
+++ doitjava/src/chapter04/IfEx2.java
@@ -7,12 +7,18 @@ public class IfEx2 {
 		if (score >= 90) {
 			System.out.println("점수가 90보다 크거나 같습니다.");
 			System.out.println("등급은 A입니다.");
-		} else if(score >= 80) {	//else 구문은 if문에서 false일 경우 실행되는 영역
+		} else if (score >= 80) { // else 구문은 if문에서 false일 경우 실행되는 영역
 			System.out.println("점수가 80보다 크거나 같습니다.");
 			System.out.println("등급은 B입니다.");
-		} else if(score >= 70) {	//else 구문은 if문에서 false일 경우 실행되는 영역
-			System.out.println("점수가 70보다 크거나 같습니다.");
-			System.out.println("등급은 C입니다.");
+		} else if (score >= 70) { // else 구문은 if문에서 false일 경우 실행되는 영역
+			if (score >= 75) {
+				System.out.println("점수가 75보다 크거나 같습니다.");
+				System.out.println("등급은 C+입니다.");
+			} else {
+				System.out.println("점수가 70보다 크거나 같습니다.");
+				System.out.println("등급은 C입니다.");
+			}
+
 		} else {
 			System.out.println("등급은 F입니다.");
 		}
