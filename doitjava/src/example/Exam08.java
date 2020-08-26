cdbbf0516ec9fc47e2e00576a64c05a6959fde3a
namwookyung <namwookyung.dev@gmail.com>
2020-08-26 15:20:08 +0900

    1에서 10까지 정순, 역순 출력

---
+++ doitjava/src/example/Exam08.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Exam08 {
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		System.out.println();
+		
+		for(int i=10; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+}
