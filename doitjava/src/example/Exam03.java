cf5d9f70a43d1f42a6d418617dfaee23a2feafe8
namwookyung <namwookyung.dev@gmail.com>
2020-08-26 14:56:52 +0900

    for문

---
+++ doitjava/src/example/Exam03.java
@@ -0,0 +1,13 @@
+package example;
+
+public class Exam03 {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1; i<=10; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1부터 10까지의 합은 " + sum + "입니다.");
+	}
+}
