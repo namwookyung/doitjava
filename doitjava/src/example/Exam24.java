1bca6724434f52da68c048270bcf1c44e3bbc3e3
namwookyung <namwookyung.dev@gmail.com>
2020-09-03 15:19:06 +0900

    for문과 if-else문으로 역삼각형 출력

---
+++ doitjava/src/example/Exam24.java
@@ -0,0 +1,16 @@
+package example;
+
+public class Exam24 {
+	public static void main(String[] args) {
+		for (int i = 5; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				if (i == j) {
+					System.out.print(i);
+				} else {
+					System.out.print("@");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
