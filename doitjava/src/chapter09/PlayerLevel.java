6473df144b3c3fd32ad5140952c096c3bd8b21b5
namwookyung <namwookyung.dev@gmail.com>
2020-09-16 13:49:44 +0900

    템플릿 메소드 예제

--- doitjava/src/chapter09/PlayerLevel.java
+++ doitjava/src/chapter09/PlayerLevel.java
@@ -2,13 +2,16 @@ package chapter09;
 
 public abstract class PlayerLevel {
 	public abstract void run();
+
 	public abstract void jump();
+
 	public abstract void turn();
+
 	public abstract void showLevelMessage();
-	
+
 	final public void go(int count) {
 		run();
-		for(int i=0; i < count; i++) {
+		for (int i = 0; i < count; i++) {
 			jump();
 		}
 		turn();
