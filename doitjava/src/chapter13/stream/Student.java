e0ecca9b305457869fa241ac1fdda244c0cb25e6
namwookyung <namwookyung.dev@gmail.com>
2020-09-29 14:43:07 +0900

    sorting

--- doitjava/src/chapter13/stream/Student.java
+++ doitjava/src/chapter13/stream/Student.java
@@ -1,6 +1,6 @@
 package chapter13.stream;
 
-public class Student {
+public class Student implements Comparable<Student> {
 	private String name;
 	private int score;
 
@@ -21,5 +21,10 @@ public class Student {
 	public String toString() {
 		// TODO Auto-generated method stub
 		return super.toString();
+	}
+	
+	@Override	//Comparable 인터페이스의 추상 메소드
+	public int compareTo(Student o) {
+		return Integer.compare(score, o.score);	//매개값을 비교하여 -1, 0, 1을 반환한다.
 	}
 }
